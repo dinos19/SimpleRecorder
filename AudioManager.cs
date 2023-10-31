e8dc8043cee596bde973ce73c0b24327cf8e05a7
kwsta <kwsta@DESKTOP-K1CJ8IT>
2023-10-31 18:04:44 +0200

    Add project files.

---
+++ AudioManager.cs
@@ -0,0 +1,92 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudio.Wave.Compression;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRecorder
+{
+    public class AudioManager
+    {
+        public string SaveFilePath { get; set; }
+        public string MyNewAudioFileName { get; set; }
+        private WaveInEvent waveSource = null;
+
+        private WaveFileWriter waveFile = null;
+
+        public void InitRecorder()
+        {
+            SaveFilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\MyRecordedWaves\";
+            if (!Directory.Exists(SaveFilePath))
+                Directory.CreateDirectory(SaveFilePath);
+        }
+
+        public void OpenRecordedAudioFolder() => Process.Start("explorer.exe", SaveFilePath);
+
+        public void OpenRecordedAudioFile()
+        {
+            if (Directory.Exists(SaveFilePath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = MyNewAudioFileName,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+        }
+
+        public void StartRecording()
+        {
+            MyNewAudioFileName = SaveFilePath + new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() + ".wav";
+            waveSource = new WaveInEvent();
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter(MyNewAudioFileName, waveSource.WaveFormat);
+            waveSource.StartRecording();
+        }
+
+        private void waveSource_RecordingStopped(object? sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+        }
+
+        private void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            //WaveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+
+            //2
+
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        public void StopRecording()
+        {
+            if (waveSource != null)
+            {
+                waveSource.StopRecording();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using SimpleRecorder;
+
+AudioManager audio = new AudioManager();
+audio.InitRecorder();
+MainAppLoop();
+void MainAppLoop()
+{
+    Console.WriteLine("Choose an action.\n1) Open recorded audios folder\n2) Record a new audio\n3) Exit");
+    var startingOption = Console.ReadLine();
+    switch (startingOption)
+    {
+        case "1":
+            audio.OpenRecordedAudioFolder();
+            break;
+
+        case "2":
+            audio.StartRecording();
+            Console.WriteLine("Starting recording.. Press anything to stop it.");
+
+            Console.ReadLine();
+
+            audio.StopRecording();
+            break;
+
+        case "3":
+            return;
+    };
+
+    SecondaryAppLoop();
+}
+
+void SecondaryAppLoop()
+{
+    Console.WriteLine("Choose an action.\n1) Play recorded\n2) Start over\n3) Exit");
+
+    var afterRecordOptions = Console.ReadLine();
+    switch (afterRecordOptions)
+    {
+        case "1":
+            audio.OpenRecordedAudioFile();
+            break;
+
+        case "3":
+            return;
+    };
+
+    MainAppLoop();
+}
